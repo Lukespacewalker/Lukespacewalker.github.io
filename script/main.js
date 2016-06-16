d56eef687378e80c003089c74ed70731ee500514
Suttisak <suttisak.lukesp@outlook.com>
2016-06-16 12:39:18 +0700

    Update UI

--- script/main.js
+++ script/main.js
@@ -14,6 +14,9 @@
      this.appendChild(sectionTitle);
    }
   }
+  var galleryPosition;
+  var target;
+  var animatedFrameID = null;
    class ImageGallery extends HTMLDivElement{
        constructor() {super()}
        attachedCallback (){
@@ -79,32 +82,65 @@
            }
            let class_gallery = document.querySelector(".gallery");
            imageCollecton = null; 
+           var currentImage = null;
+           var isCollpase = true;
            var fullimg = document.createElement("div");
            viewer.appendChild(fullimg);
            filmstrip.addEventListener("click",function(evt){
                event.preventDefault();
+               if(currentImage != event.target || isCollpase){
+               currentImage = event.target;
+               galleryPosition = class_gallery.getBoundingClientRect();
+               target = window.pageYOffset + galleryPosition.top
+               animatedFrameID = window.requestAnimationFrame(scroll);
                class_gallery.style.minHeight = "100vh";
-               
                filmstrip.classList.add("small")
-               setTimeout(function(){viewer.classList.add("show");},520);
+               setTimeout(function(){viewer.classList.add("show");isCollpase = false;},520);
                let fullpath = event.target.dataset.fullpath;
                if(fullpath != null || fullpath != undefined){
                    fullimg.style.backgroundImage = `url("`+fullpath+`")`;
                }
+               
+               }
+               else{
+               viewer.classList.remove("show");
+               class_gallery.style.minHeight = "0vh";
+               filmstrip.classList.remove("small")
+               isCollpase = true;;
+               }
            },false);
        }
+   }
+
+   var scrollStart = null;
+   
+   function scroll(timestamp){
+       const animateTime = 1000;
+       if(!scrollStart){scrollStart = timestamp;}
+       let progress = timestamp - scrollStart;
+       window.scrollTo(window.pageXOffset,window.pageYOffset + progress*(target-window.pageYOffset)/animateTime);
+        if (progress < animateTime) {
+    window.requestAnimationFrame(scroll);
+  }else{
+      window.cancelAnimationFrame(animatedFrameID);
+      scrollStart = null;
+  }
    }
    class ArticleGroup extends HTMLDivElement{
        constructor(){
            super();
        }
         attachedCallback (){
-            this.style.display = "block";
+            this.style.display = "inline-block";
             // Get Articles Group Name
-            let headerNode = this.parentNode.querySelector("header");
+            let headerNode = this.parentNode.parentNode.querySelector("header");
+
             let navNode = headerNode.querySelector("nav");
+            
             if(navNode==null){navNode = document.createElement("nav");}
+
             headerNode.appendChild(navNode);
+            
             let buttonNode = document.createElement("button");
             buttonNode.textContent = this.dataset.name;
             navNode.appendChild(buttonNode);
@@ -118,3 +154,17 @@
   var gallery_group = document.registerElement("gallery-group",Object.create(HTMLElement.prototype));
   var article_group = document.registerElement("article-group",ArticleGroup);
     var outer_container = document.registerElement("outer-container", Object.create(HTMLDivElement.prototype));
+
+document.addEventListener("DOMContentLoaded",function(){
+       let mainnav = document.querySelector("#mainnav");
+       mainnav.addEventListener("click",function(evt){
+           evt.preventDefault();
+           let jumpTarget = evt.target.dataset.target;
+           if(jumpTarget != null || jumpTarget != ""){
+               let targetPosition = document.querySelector(jumpTarget);
+               targetPosition = targetPosition.getBoundingClientRect();
+               target = window.pageYOffset + targetPosition.top;
+               animatedFrameID = window.requestAnimationFrame(scroll);
+           }
+       },false);
+   });
